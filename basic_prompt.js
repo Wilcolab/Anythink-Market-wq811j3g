c7f683b600fb7c1e12ff498f6be911bf2d15b294
WilcoApp <hello@wilco.gg>
2025-10-15 16:11:38 +0000

    Initial commit

--- basic_prompt.js
+++
@@ -1,7 +0,0 @@
-/**
- * Converts a string to camelCase.
- * Example: "hello world" => "helloWorld"
- * Example: "convert_this-string" => "convertThisString"
- * @param {string} str - The input string to convert.
- * @returns {string} The camelCase version of the input.
- */
